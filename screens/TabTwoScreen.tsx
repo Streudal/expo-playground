b9dc4f09df37b30d331fcf07fef2551a8f42b00e
Tyler Streu <streuta@cdmsmith.com>
2021-11-07 16:56:23 -0600

    Got iOS and Android build working on expo with zustand and react-native-mapbox-gl.

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,32 +1,23 @@
+import MapboxGL from '@react-native-mapbox-gl/maps';
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import { View } from '../components/Themed';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+MapboxGL.setAccessToken('pk.eyJ1Ijoic3RyZXV0YSIsImEiOiJja2c4OWo3emcwYmFxMndzNXFpZXU4Y3A1In0.YWRAPmIkJj6ry8S3gmna6w');
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    <View style={styles.page}>
+      <MapboxGL.MapView style={styles.map} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  page: {
+    flex: 1
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  map: {
+    flex: 1
+  }
 });
